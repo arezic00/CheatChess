6b7b424b52dc3593e2500e28945e8170e4d81b49
arezic00 <149725228+arezic00@users.noreply.github.com>
2023-11-19 16:03:55 +0100

    made drawSquareAt() and drawBoard() function
    This change makes it a lot more intuitive i think

--- app/src/main/java/com/example/cheatchess/ChessView.kt
+++ app/src/main/java/com/example/cheatchess/ChessView.kt
@@ -6,25 +6,37 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 
 class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private val paint = Paint()
+    private val darkColor = ContextCompat.getColor(context!!,R.color.darkSquare)
+    private val lightColor = ContextCompat.getColor(context!!,R.color.lightSquare)
     private val rectSide = 130f
     private val marginLeft = 20f
     private val marginTop = 200f
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        drawBoard(canvas)
+    }
+
+    private fun drawBoard(canvas: Canvas?) {
         for (row in 0..7)
             for (col in 0..7) {
-                paint.color = if ((row + col) % 2 == 0) Color.LTGRAY else Color.GRAY
-                canvas?.drawRect(
-                    marginLeft + rectSide*col,
-                    marginTop + rectSide*row,
-                    marginLeft + rectSide*(col+1),
-                    marginTop + rectSide*(row+1),
-                    paint)
+                drawSquareAt(canvas, row, col)
             }
-
     }
+
+    private fun drawSquareAt(canvas: Canvas?, row: Int, col: Int) {
+        paint.color = if ((row + col) % 2 == 0) lightColor else darkColor
+        canvas?.drawRect(
+            marginLeft + rectSide*col,
+            marginTop + rectSide*row,
+            marginLeft + rectSide*(col+1),
+            marginTop + rectSide*(row+1),
+            paint)
+    }
+
+    
 }
